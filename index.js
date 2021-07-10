26aaec082aac572ee16819d91ec47f1333177afe
Hatice <haticekaraty@gmail.com>
2021-07-09 22:30:19 -0400

    commit tree 9a8321fab7fa8b1366cc21baac0b18b445460742

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(array,gift) {
+    let newarray = []
+    for (let i = 0; i < array.length; i++){
+        let message = `Thank you, ${array[i]}, for the wonderful ${gift} gift!`
+        newarray.push(message);
+        //console.log(message);
+    }
+    return newarray;
+}
+//let number = 10
+function countDown(number){
+    for(let i = number; i>=0; i--){
+        console.log(i);
+    }
+}
+
+//countDown(number);
